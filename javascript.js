3c341817182523ac752e5e8f8f306168d5d27da5
ryanproj <100888918+ryanproj@users.noreply.github.com>
2022-03-04 00:54:11 -0500

    more updates

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var json = {"d":[  
+var professions = {"d":[  
 			{"profession":"Berserker"},
 			{"profession":"Shadow Hunter"},
 			{"profession":"Gunslinger"},
@@ -18,7 +18,27 @@ var json = {"d":[
 
 $(document).ready(function() {
     var $grouplist = $('#checkboxes');
-    $.each(json.d, function() {
-        $('<label>'+this.profession+': </label><input type=checkbox value='+this.profession+'/>').appendTo($grouplist);
+    $.each(professions.d, function() {
+        $('<label><input type=checkbox name='+this.profession+' value='+this.profession+'/> ' +this.profession+'</label>').appendTo($grouplist);
     });
 });
+
+// called onclick of  beauty products checkboxes
+function updateCost(e) {
+  // 'this' reffered to checkbox clicked on
+  var myForm = this.form;
+  alert('event fired');
+
+  // include current value in total-cost block, use parseFloat method to convert string to number
+  // var val = parseFloat(myForm.elements['total-cost'].value);
+
+  // Add the checkbox value to total value if checkbox is checked
+  if (this.checked) {
+    alert('checked' + this.value);
+  } else {
+    alert('unchecked' + this.value);
+  }
+
+  // update total-cost value with latest value
+  //myForm.elements['total-cost'].value = val
+}
