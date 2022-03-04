66981f0ccfb2bc9710f42b1fd5683389ccf4fa61
ryanproj <100888918+ryanproj@users.noreply.github.com>
2022-03-03 23:44:05 -0500

    aded js file and updated index

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
+var json = {"d":[  {"__type":"Service1.Operators:#MobileWCFService","operators":"ACCOUNTING"},   {"__type":"Service1.Operators:#MobileWCFService","operators":"AHOFFMAN"},   {"__type":"Service1.Operators:#MobileWCFService","operators":"AMONROY"},   {"__type":"Service1.Operators:#MobileWCFService","operators":"APATENTAS"},
+    {"__type":"Service1.Operators:#MobileWCFService","operators":"WRAMOS    "}
+]};
 
+$(document).ready(function() {
+    var $grouplist = $('#checkboxes');
+    $.each(json.d, function() {
+        $('<label>'+this.operators+': </label><input type=checkbox value='+this.operators+'/>').appendTo($grouplist);
+    });
+});
